dd65c78b3aa4f8ea08aeb7493dc02e3ff3b14e6d
frazynondo <Eugene@905>
2021-01-08 11:52:54 -0500

    Some Interview Algo

--- src/Graphs/GraphHashMapAdj.java
+++ src/Graphs/GraphHashMapAdj.java
@@ -17,7 +17,7 @@ public class GraphHashMapAdj {
         adjListMap = new HashMap<>();
 //        System.out.println(vertices_num + " vertices, " + Edges_num + " Edges ");
 
-        //Assign each vertice a List where connecting nodes will be added
+        //Assign each Vertice a List with neighbouring Nodes
         for(int i = 0; i<vertices_num; i++){
             ArrayList<Integer> neghbours = new ArrayList<>();
             adjListMap.put(i, neghbours);
